98b0d8b8b93d72746ea043cf6ec467ebe4f2a8cd
Msgun <msgunberhe24@gmail.com>
2024-12-14 13:10:37 -0600

    Remove empty submodule frontend

---
+++ MyfullStackProject-Backend/src/test/java/com/msgun/myfullstackproject/MyfullStackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msgun.myfullstackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfullStackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
